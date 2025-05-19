d4957dd5cc8a8db116cde12af930a37dea21c5ca
PC <winniewaiguru@gmail.com>
2025-05-19 20:55:23 +0300

    Updated 1-multi-languages.js

--- 1-multi-languages.js
+++ 1-multi-languages.js
@@ -1,3 +1,7 @@
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+let firstLine = "C is fun";
+let secondLine = "Python is cool";
+let thirdLine = "JavaScript is amazing";
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
