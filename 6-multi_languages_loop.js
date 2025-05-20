bd1cbc2b4840f951a271ef7ca588b680be606c16
PC <winniewaiguru@gmail.com>
2025-05-20 09:05:22 +0300

    Added 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+let arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let output = "";
+
+for (let index = 0; index < arr.length; index++) {
+  output += arr[index] + "\n";
+}
+
+console.log(output.trim());
