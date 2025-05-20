e28b6fbe463fd9a9873ff498a5d1752ad0be50ba
PC <winniewaiguru@gmail.com>
2025-05-20 06:56:37 +0300

    Added  9-add.js

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  console.log(a + b);
+}
+
+const firstNum = prompt("Enter the first number");
+const secondNum = prompt("Enter the first number");
+
+const num1 = parseInt(firstNum);
+const num2 = parseInt(secondNum);
+
+add(num1, num2);
